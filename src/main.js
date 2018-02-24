a15c910d16f63663059b509890b332960bf350e7
Gerry Mc Donnell <irishbloke@gmail.com>
2018-02-24 19:24:29 +0000

    interceptors

--- src/main.js
+++ src/main.js
@@ -8,6 +8,22 @@ import store from './store'
 //default settings for axios
 axios.defaults.baseURL='https://vueaxios-7085c.firebaseio.com'
 
+//setup headers if needed
+/*axios.defaults.headers.common['Authorization']='this is my auth string'
+axios.defaults.headers.get['Accepts']='application/json'*/
+
+
+//axios.interceptors
+axios.interceptors.request.use(config=>{
+  console.log('[request] interceptor',config)
+  return config
+})
+
+axios.interceptors.response.use(res=>{
+  console.log('[response] interceptor',res)
+  return res
+})
+
 new Vue({
   el: '#app',
   router,
