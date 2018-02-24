1211bbe9c88a91b350443d71d737f6cb043725e6
Gerry Mc Donnell <irishbloke@gmail.com>
2018-02-24 18:30:52 +0000

    firebase get request

--- src/components/dashboard/dashboard.vue
+++ src/components/dashboard/dashboard.vue
@@ -2,6 +2,9 @@
   <div id="dashboard">
     <h1>That's the dashboard!</h1>
     <p>You should only get here if you're authenticated!</p>
+
+    <p>Your email address is {{email}}</p>
+
   </div>
 </template>
 
@@ -10,11 +13,32 @@
 import axios from 'axios'
 
   export default{
+    data(){
+      return{
+        email:'me@me.com'
+      }
+    },
+
     //created is a lifecycle hook of vuejs
     created(){
       //send get request
       axios.get('https://vueaxios-7085c.firebaseio.com/users.json')
-      .then(res=>console.log(res))
+      .then(res=>{
+        console.log(res)
+
+        //res.data is whats returned by backend
+        const data=res.data
+        const users=[]
+
+        for(let key in data){
+          const user=data[key]
+          user.id=key
+          users.push(user)
+        }
+
+        console.log(users)
+        this.email=users[0].email
+      })
       .catch(error=>console.log(error))
     }
   }
